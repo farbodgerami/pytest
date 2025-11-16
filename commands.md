a95bfa89ef93a0164ba13ca888847adf2ba0f7a7
farbodgerami <fb.gerami@gamil.com>
2025-11-16 22:40:52 +0330

    first

--- commands.md
+++ commands.md
@@ -1,4 +1,11 @@
 pytest .
 
-more specific:
+### more specific:
 pytest ./gist/test_gist.py -v
+
+ 
+### run with a specific mark:
+pytest ./gist/test_gist.py -v -p no:warnings -m slow
+
+### exclude a specific mark:
+pytest ./gist/test_gist.py -v -p no:warnings -m "not slow"
--- gist/test_gist.py
+++ gist/test_gist.py
@@ -1,10 +1,23 @@
 import pytest
+
 def test_our_first_test() -> None:
-    assert 1==1
+    assert 1 == 1
 
 
 # test must be skipped:
 @pytest.mark.skip
 def test_should_be_skipped() -> None:
-  assert 1==2
+  assert 1 == 2
 
+@pytest.mark.skipif(4 >1,reason="skipped becouse 4>1")
+def test_should_be_skipped_if()-> None:
+    assert 1 == 2
+    
+# teset we are oke with them of failing
+@pytest.mark.xfail
+def test_dont_care_if_fails() -> None:
+    assert 1==2
+    
+@pytest.mark.slow
+def test_slow() -> None:
+    pass
